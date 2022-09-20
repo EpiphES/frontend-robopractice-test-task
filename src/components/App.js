87ddda8da41527ddce38fd4c520cb863c55d9411
Ekaterina Epiphanova <epiph@yandex.ru>
2022-09-20 13:55:42 +0500

    feat: enable search

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 import api from '../utils/api';
+import Button from 'react-bootstrap/Button';
+
+import closeIcon from '../images/close.svg';
 
 function App() {
   const [searchQuery, setSearchQuery] = useState('');
   const [users, setUsers] = useState([]);
+  const [searchedUsers, setSearchedUsers] = useState(users);
 
   function handleInputChange(e) {
     setSearchQuery(e.target.value);
@@ -17,27 +22,43 @@ function App() {
       setUsers(data);
     })
     .catch(err => console.log(err));
-  }  
+  }
+  
+  function resetInput() {
+    setSearchQuery("");
+  }
   
   useEffect(() => {
     loadUsersData();
   }, [])
 
-  console.log(users);
+  useEffect(() => {
+    setSearchedUsers(users.filter(user => user.Fullname.toLowerCase().includes(searchQuery.toLowerCase()))
+    );       
+  },[users, searchQuery]); 
+  
+  console.log(searchedUsers);
 
   return (
     <div className="App">
       <Container fluid="md" className="py-3">
-        <Form.Control
-              type="text"
-              placeholder="Search users"
-              name="search"
-              id="search-input"
-              value={searchQuery}
-              onInput={(e) => handleInputChange(e)}
-            />
-      </Container>
-      
+        <InputGroup className="mb-3">
+          <Form.Control
+                type="text"
+                placeholder="Search users"
+                name="search"
+                id="search-input"
+                value={searchQuery}
+                onInput={(e) => handleInputChange(e)}
+          />
+          <Button 
+            variant="outline-secondary" aria-label="reset input"
+            onClick={() => resetInput()}
+          >
+            <img src={closeIcon} alt="left arrow icon" centered />
+          </Button>
+        </InputGroup>       
+      </Container>     
     </div>
   );
 
